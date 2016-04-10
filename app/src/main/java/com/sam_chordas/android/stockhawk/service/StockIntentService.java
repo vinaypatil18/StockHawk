e9c976101d406aaa2a7a1080434d08c88ccc0b13
vinaypatil18 <vinay18.patil@gmail.com>
2016-04-10 23:24:26 +0530

    Handle Invalid Stock Symbol

--- app/src/main/java/com/sam_chordas/android/stockhawk/service/StockIntentService.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/service/StockIntentService.java
@@ -3,7 +3,11 @@ package com.sam_chordas.android.stockhawk.service;
 import android.app.IntentService;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
+import android.widget.Toast;
+
 import com.google.android.gms.gcm.TaskParams;
 
 /**
@@ -11,23 +15,35 @@ import com.google.android.gms.gcm.TaskParams;
  */
 public class StockIntentService extends IntentService {
 
-  public StockIntentService(){
-    super(StockIntentService.class.getName());
-  }
-
-  public StockIntentService(String name) {
-    super(name);
-  }
-
-  @Override protected void onHandleIntent(Intent intent) {
-    Log.d(StockIntentService.class.getSimpleName(), "Stock Intent Service");
-    StockTaskService stockTaskService = new StockTaskService(this);
-    Bundle args = new Bundle();
-    if (intent.getStringExtra("tag").equals("add")){
-      args.putString("symbol", intent.getStringExtra("symbol"));
+    public StockIntentService() {
+        super(StockIntentService.class.getName());
     }
-    // We can call OnRunTask from the intent service to force it to run immediately instead of
-    // scheduling a task.
-    stockTaskService.onRunTask(new TaskParams(intent.getStringExtra("tag"), args));
-  }
+
+    public StockIntentService(String name) {
+        super(name);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Log.d(StockIntentService.class.getSimpleName(), "Stock Intent Service");
+        StockTaskService stockTaskService = new StockTaskService(this);
+        Bundle args = new Bundle();
+        if (intent.getStringExtra("tag").equals("add")) {
+            args.putString("symbol", intent.getStringExtra("symbol"));
+        }
+        // We can call OnRunTask from the intent service to force it to run immediately instead of
+        // scheduling a task.
+        int result = stockTaskService.onRunTask(new TaskParams(intent.getStringExtra("tag"), args));
+        if (result == -1) {
+            //invalid symbol entered by user
+            Handler handler=new Handler(Looper.getMainLooper());
+            handler.post(new Runnable(){
+                public void run(){
+                    Toast.makeText(getApplicationContext(), "Enter Valid Stock Symbol", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
+
 }
